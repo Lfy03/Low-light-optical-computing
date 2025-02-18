3c446e7839c9758fe4c31e28d0dd1949bb294c03
Lfy03 <findyourlife@FindYourLife.>
2025-02-18 17:35:35 +0800

    update

--- train.py
+++ train.py
@@ -1,6 +1,2 @@
 import torchvision
 import torchvision.transforms as transforms
-
-# 下载 MNIST 数据集
-mnist_train = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transforms.ToTensor())
-mnist_test = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transforms.ToTensor())
